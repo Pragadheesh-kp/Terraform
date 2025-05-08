6f2ce37a7c92d8a63ca679157b11d988ac767827
Pragadheesh-kp <138524264+Pragadheesh-kp@users.noreply.github.com>
2025-05-08 17:33:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-2"  # Replace with your desired region
+  region = "ap-south-1"  # Replace with your desired region
 }
 
 resource "aws_instance" "prgade_instance" {
