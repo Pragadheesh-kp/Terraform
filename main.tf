b1f3db7837e719eae46fab8c6402137fa8ecc3ca
Pragadheesh-kp <138524264+Pragadheesh-kp@users.noreply.github.com>
2025-05-09 10:09:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"  # Replace with your desired region
+  region = "ap-south-1" # Replace with your desired region
 }
 
 resource "aws_instance" "prgade_instance" {
@@ -12,6 +12,31 @@ resource "aws_instance" "prgade_instance" {
   tags = {
     Name = "prgade-instance1"
   }
+
+  # User data to install Docker, create directory, and clone repositories
+  user_data = <<-EOF
+              #!/bin/bash
+              # Update and install required packages
+              sudo apt-get update -y
+              # Install Docker
+              sudo apt-get install -y docker.io
+
+              # Enable and start Docker service
+              sudo systemctl enable docker
+              sudo systemctl start docker
+
+              # Add ubuntu user to Docker group
+              sudo usermod -aG docker ubuntu
+
+              # Create the directory
+              mkdir -p /home/ubuntu/docker
+
+              # Navigate to the directory
+              cd /home/ubuntu/docker
+
+              # Pull the two repositories
+              git clone https://github.com/Pragadheesh-kp/docker.git
+              EOF
 }
 
 data "aws_security_group" "existing_sg" {
